899b308624b4eeabef7faf96dda13536bc87d740
slowDony <435574793@qq.com>
2017-09-01 11:41:33 +0800

    SDCityPicker_Swift初始化

---
+++ SDCityPicker_Swift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SDCityPicker_Swift
+//
+//  Created by slowdony on 2017/8/30.
+//  Copyright © 2017年 SlowDony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
